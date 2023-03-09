39bd776ee3f2ab3791362991e7d64262042c0b84
Kasper Adriaensen <kasper@lugus-studios.be>
2023-03-09 20:38:01 +0100

    Removed some old example API functions

--- Pinion/Assets/Examples/DroneFlight/API/DroneAPI.cs
+++ Pinion/Assets/Examples/DroneFlight/API/DroneAPI.cs
@@ -1,31 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Pinion;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Pinion;
 
-[APISource]
-public static class DroneAPI
-{
-	[APIMethod]
-	public static float GetDroneHeightAbsolute()
-	{
-		GameObject drone = GameObject.Find("POCDrone");
+// [APISource]
+// public static class DroneAPI
+// {
+// 	[APIMethod]
+// 	public static float GetDroneHeightAbsolute()
+// 	{
+// 		GameObject drone = GameObject.Find("POCDrone");
 
-		if (drone != null)
-			return drone.transform.position.y;
+// 		if (drone != null)
+// 			return drone.transform.position.y;
 
-		return 0f;
-	}
+// 		return 0f;
+// 	}
 
-	[APIMethod]
-	public static void DisplayMessage(string message, string id)
-	{
-		ExampleGameUI.Instance.DisplayMessage(message, id);
-	}
+// 	[APIMethod]
+// 	public static void DisplayMessage(string message, string id)
+// 	{
+// 		ExampleGameUI.Instance.DisplayMessage(message, id);
+// 	}
 
-	[APIMethod]
-	public static void RemoveMessage(string id)
-	{
-		ExampleGameUI.Instance.RemoveMessage(id);
-	}
-}
+// 	[APIMethod]
+// 	public static void RemoveMessage(string id)
+// 	{
+// 		ExampleGameUI.Instance.RemoveMessage(id);
+// 	}
+// }
